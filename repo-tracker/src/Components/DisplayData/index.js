3178d75b3ae943ddda9bd0ad66574a8ecd419aea
Ben <bentidball97@gmail.com>
2022-07-14 10:35:24 +0100

    icons, onclick and css changes

--- repo-tracker/src/Components/DisplayData/index.js
+++ repo-tracker/src/Components/DisplayData/index.js
@@ -1,17 +1,20 @@
 import React from "react";
 
 function DisplayData({apiData}){
-
     const results = [];
+
 
     if(apiData.length > 0){
         apiData.forEach(repo => {
+            console.warn(repo);
             results.push(
-                <li key={repo.id} className='repo'>
+                <li key={repo.id} className='repo' onClick={()=> {
+                        window.open(repo.html_url, '_blank');
+                    }}>
                     <h1>{repo.name}</h1>
-                    <p>Forks: {repo.forks}</p>
-                    <p>stargazers: {repo.stargazers_count}</p>
-                    <p>watchers: {repo.watchers_count}</p>
+                    <i className="fa-solid fa-code-fork"> {repo.forks} </i>
+                    <i className="fa-solid fa-star"> {repo.stargazers_count} </i> 
+                    <i className="fa-solid fa-eye"> {repo.watchers_count} </i> 
                 </li>
             );
         });
